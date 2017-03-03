dc6243497e4e4f19f0d5fab6dfe43b52c02753e2
Oliver Walters <oliver.henry.walters@gmail.com>
2017-03-03 15:00:14 +1100

    Added travis CI scripts

--- library-check.sh
+++ library-check.sh
@@ -2,7 +2,8 @@ SCRIPT="/home/travis/build/kicad-library-utils/pcb/check_kicad_mod.py"
 
 error=0
 
-for change in $(git diff --name-only --diff-filter=AM HEAD...master); do
+for change in $(git diff --name-only --diff-filter=AM $TRAVIS_COMMIT_RANGE); do
+    echo $change
     python3 $SCRIPT $change -vv
     error="$(($error+$?))"
 done
