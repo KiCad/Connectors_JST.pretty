afcf0110e373d03dad452f18ce09b81e62920eab
Oliver Walters <oliver.henry.walters@gmail.com>
2017-03-03 14:53:51 +1100

    Added travis CI scripts

---
+++ library-check.sh
@@ -0,0 +1,10 @@
+SCRIPT="/home/travis/build/kicad-library-utils/pcb/check_kicad_mod.py"
+
+error=0
+
+for change in $(git diff --name-only --diff-filter=AM HEAD...master); do
+    python3 $SCRIPT $change -vv
+    error="$(($error+$?))"
+done
+
+exit $error
